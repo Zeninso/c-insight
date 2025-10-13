5fb87358a45d53a8cc0e2433ffca058aac5c798a
vincent-villas <vincentvillas0208@gmail.com>
2025-10-14 00:36:17 +0800

    I CREATE THE CONNECTION OF THE DATABASE

--- app/__init__.py
+++ app/__init__.py
@@ -5,41 +5,49 @@ import os
 from dotenv import load_dotenv
 import time
 
+# Load environment variables from .env
 load_dotenv()
+
+# Initialize MySQL (will be attached to app later)
 mysql = MySQL()
 
 def create_app():
     app = Flask(__name__, static_folder='static')
+
+    # Secret key
     app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'dev-secret-key-change-in-production')
 
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = os.environ.get('OAUTHLIB_INSECURE_TRANSPORT', '0')
+    # --- MySQL Railway Config ---
+    app.config['MYSQL_HOST'] = os.environ.get('MYSQLHOST')            # Railway host
+    app.config['MYSQL_USER'] = os.environ.get('MYSQLUSER')            # Railway username
+    app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQLPASSWORD')    # Railway password
+    app.config['MYSQL_DB'] = os.environ.get('MYSQLDATABASE')          # Railway database
+    app.config['MYSQL_PORT'] = int(os.environ.get('MYSQLPORT', 3306)) # Railway port
+    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'                    # return dict instead of tuple
+    app.config['MYSQL_USE_UNICODE'] = True                             # REQUIRED for Flask-MySQLdb
 
-    # Google OAuth setup
+    # Attach MySQL to app
+    mysql.init_app(app)
+
+    # --- Google OAuth setup ---
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = os.environ.get('OAUTHLIB_INSECURE_TRANSPORT', '0')
     google_bp = make_google_blueprint(
         client_id=os.environ.get('GOOGLE_CLIENT_ID'),
         client_secret=os.environ.get('GOOGLE_CLIENT_SECRET'),
-        scope=["https://www.googleapis.com/auth/userinfo.profile",
-               "https://www.googleapis.com/auth/userinfo.email",
-               "openid"]
+        scope=[
+            "https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email",
+            "openid"
+        ]
     )
     app.register_blueprint(google_bp, url_prefix="/login")
 
+    # Connect Flask-Dance signal for Google login
     from routes.auth import google_logged_in
     from flask_dance.consumer import oauth_authorized
     oauth_authorized.connect_via(google_bp)(google_logged_in)
 
-    # --- MySQL Private Config ---
-    app.config['MYSQL_HOST'] = os.environ.get('MYSQLHOST')
-    app.config['MYSQL_USER'] = os.environ.get('MYSQLUSER', 'root')
-    app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQLPASSWORD')
-    app.config['MYSQL_DB'] = os.environ.get('MYSQLDATABASE')
-    app.config['MYSQL_PORT'] = int(os.environ.get('MYSQLPORT') or 3306)
-    app.config['MYSQL_UNIX_SOCKET'] = None
-    app.config['MYSQL_CONNECT_TIMEOUT'] = None
-    app.config['MYSQL_READ_DEFAULT_FILE'] = None
-
-
-    # Register blueprints
+    # --- Register Blueprints ---
     from routes.home import home_bp
     from routes.auth import auth_bp
     from routes.teacher import teacher_bp
