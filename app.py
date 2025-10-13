795851012e2499938177e2ab6b296a5ea6879c06
vincent-villas <vincentvillas0208@gmail.com>
2025-10-13 19:02:08 +0800

    I FIXED SOME ISSUES

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ import os
 from app import create_app
 
 app = create_app()
+application = app  # For Gunicorn
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
-
-# For Railway deployment
-application = app
