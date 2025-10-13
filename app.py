1931caf11fe0149a0b6534610eea58903485add5
vincent-villas <vincentvillas0208@gmail.com>
2025-10-13 19:31:24 +0800

    I FIXED SOME ISSUES

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def home():
     return 'C-Insight with GCC is working!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port, debug=False)
